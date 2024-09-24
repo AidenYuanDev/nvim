fb71f8f63357b2609d3ce8625756de2073944ded
aiden <aiden.yuan.dev@gmail.com>
2024-09-23 20:35:40 -0400

    close format_on_save

--- lua/aiden/plugins/formatting.lua
+++ lua/aiden/plugins/formatting.lua
@@ -24,11 +24,11 @@ return {
 				cpp = { "clang-format" },
 				cmake = { "cmake_format" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
+			-- format_on_save = {
+			-- 	lsp_fallback = true,
+			-- 	async = false,
+			-- 	timeout_ms = 1000,
+			-- },
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
