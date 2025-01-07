7088b53a1f5f63bfeaebc2de7adb7085e06eb441
aiden <aiden.yuan.dev@gmail.com>
2025-01-07 09:35:54 -0500

    force save and quit

--- lua/aiden/core/keymaps.lua
+++ lua/aiden/core/keymaps.lua
@@ -33,3 +33,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- quick save quit
+keymap.set("n", "<leader>w", ":w!<CR>", { desc = "force save" })
+keymap.set("n", "<leader>q", ":q!<CR>", { desc = "force quit" })
