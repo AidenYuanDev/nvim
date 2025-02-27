029393e2bfdd959723543e94e9b624f1c362fad3
Aiden Yuan <Aiden.Yuan.Eng@gmail.com>
2025-02-27 09:43:14 +0800

    Toogle number and mouse

--- lua/aiden/core/keymaps.lua
+++ lua/aiden/core/keymaps.lua
@@ -37,3 +37,24 @@ keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer
 -- quick save quit
 keymap.set("n", "<leader>w", ":w!<CR>", { desc = "force save" })
 keymap.set("n", "<leader>q", ":q!<CR>", { desc = "force quit" })
+
+-- Toogle number and mouse
+local function toggle_line_numbers_and_mouse()
+	local has_number = vim.opt.number:get()
+	local has_relative = vim.opt.relativenumber:get()
+	local mouse_enabled = vim.opt.mouse:get() ~= ""
+
+	if has_number and has_relative and mouse_enabled then
+		vim.opt.relativenumber = false
+		vim.opt.number = false
+		vim.opt.mouse = ""
+	else
+		-- 当前不是完全启用状态，切换到启用状态
+		vim.opt.relativenumber = true
+		vim.opt.number = true
+		vim.opt.mouse = "a"
+	end
+end
+
+-- 将F2键绑定到切换函数
+vim.keymap.set("n", "<F2>", toggle_line_numbers_and_mouse, { desc = "Toogle number and mouse" })
