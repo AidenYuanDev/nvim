22f0b8d2d5c128d247f2e39d150bf86321fd29ca
Aiden Yuan <Aiden.Yuan.Eng@gmail.com>
2025-07-10 23:38:37 +0800

    fix bug:clipboard sync problem

--- lua/aiden/core/options.lua
+++ lua/aiden/core/options.lua
@@ -36,6 +36,18 @@ opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
