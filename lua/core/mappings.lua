8d3a2a033b4001458c5ca2d7a00164d1e4fdc6f3
Aiden Yuan <Aiden.Yuan.Eng@gmail.com>
2025-10-04 09:31:31 +0800

    feat: adjust keymapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -8,9 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-map("n", "<leader>q", "<cmd>q!<CR>", { desc = "quit force" })
-map("n", "<leader>w", "<cmd>w<CR>", { desc = "save file" })
-
 -- ═══════════════════════════════════════════════════
 -- DAP
 -- ═══════════════════════════════════════════════════
