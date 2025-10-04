c9abffea1da82d57ed56156ad6e3b50a3c164036
Aiden Yuan <Aiden.Yuan.Eng@gmail.com>
2025-10-04 10:03:43 +0800

    feat: adjust keymapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -13,51 +13,51 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 -- ═══════════════════════════════════════════════════
 map("n", "<F5>", function()
 	require("dap").continue()
-end, { desc = "debug: continue" })
+end, { desc = "Debug continue" })
 
 map("n", "<F6>", function()
 	require("dap").pause()
-end, { desc = "debug: pause" })
+end, { desc = "Debug pause" })
 
 map("n", "<F9>", function()
 	require("dap").run_to_cursor()
-end, { desc = "debug: run to cursor" })
+end, { desc = "Debug run to cursor" })
 
 map("n", "<F10>", function()
 	require("dap").step_over()
-end, { desc = "debug: step over" })
+end, { desc = "Debug step over" })
 
 map("n", "<F11>", function()
 	require("dap").step_into()
-end, { desc = "debug: step into" })
+end, { desc = "Debug step into" })
 
 map("n", "<F12>", function()
 	require("dap").step_out()
-end, { desc = "debug: step out" })
+end, { desc = "Debug step out" })
 
 map("n", "<leader>dt", function()
 	require("dap").terminate()
-end, { desc = "terminate" })
+end, { desc = "Debug terminate" })
 
 map("n", "<leader>dp", function()
 	require("dap").pause()
-end, { desc = "pause" })
+end, { desc = "Debug pause" })
 
 map("n", "<leader>db", function()
 	require("dap").toggle_breakpoint()
-end, { desc = "toggle breakpoint" })
+end, { desc = "Debug toggle breakpoint" })
 
 map("n", "<leader>dB", function()
 	require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-end, { desc = "conditional breakpoint" })
+end, { desc = "Debug conditional breakpoint" })
 
 map("n", "<leader>dib", function()
 	require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-end, { desc = "log point" })
+end, { desc = "Debug log point" })
 
 map("n", "<leader>dcb", function()
 	require("dap").clear_breakpoints()
-end, { desc = "clear breakpoints" })
+end, { desc = "Debug clear breakpoints" })
 
 map("n", "<leader>dlb", function()
 	require("dapui").float_element("breakpoints", {
@@ -67,7 +67,7 @@ map("n", "<leader>dlb", function()
 		position = "center",
 		title = "Breakpoints",
 	})
-end, { desc = "list breakpoints" })
+end, { desc = "Debug list breakpoints" })
 
 map("n", "<leader>dlr", function()
 	require("dapui").float_element("repl", {
@@ -77,7 +77,7 @@ map("n", "<leader>dlr", function()
 		position = "center",
 		title = "REPL",
 	})
-end, { desc = "float repl" })
+end, { desc = "Debug float repl" })
 
 map("n", "<leader>dlv", function()
 	require("dapui").float_element("scopes", {
@@ -87,7 +87,7 @@ map("n", "<leader>dlv", function()
 		position = "center",
 		title = "Variables",
 	})
-end, { desc = "float scopes" })
+end, { desc = "Debug float scopes" })
 
 map("n", "<leader>dls", function()
 	require("dapui").float_element("stacks", {
@@ -97,7 +97,7 @@ map("n", "<leader>dls", function()
 		position = "center",
 		title = "Call Stack",
 	})
-end, { desc = "float stacks" })
+end, { desc = "Debug float stacks" })
 
 map("n", "<leader>dlw", function()
 	require("dapui").float_element("watches", {
@@ -107,7 +107,7 @@ map("n", "<leader>dlw", function()
 		position = "center",
 		title = "Watches",
 	})
-end, { desc = "float watches" })
+end, { desc = "Debug float watches" })
 
 map("n", "<leader>dlo", function()
 	require("dapui").float_element("console", {
@@ -117,15 +117,15 @@ map("n", "<leader>dlo", function()
 		position = "center",
 		title = "Console",
 	})
-end, { desc = "float console" })
+end, { desc = "Debug float console" })
 
 map("n", "<leader>df", function()
 	require("dap").up()
-end, { desc = "frame up" })
+end, { desc = "Debug frame up" })
 
 map("n", "<leader>dF", function()
 	require("dap").down()
-end, { desc = "frame down" })
+end, { desc = "Debug frame down" })
 
 map("n", "<leader>dlt", function()
 	local dap = require("dap")
@@ -165,14 +165,14 @@ map("n", "<leader>dlt", function()
 			end
 		end)
 	end)
-end, { desc = "switch thread" })
+end, { desc = "Debug switch thread" })
 
 map({ "n", "v" }, "<leader>de", function()
 	require("dapui").eval(nil, {
 		context = "hover",
 		enter = true,
 	})
-end, { desc = "evaluate (hover)" })
+end, { desc = "Debug evaluate (hover)" })
 
 map({ "n", "v" }, "<leader>dw", function()
 	local expr
@@ -193,7 +193,7 @@ map({ "n", "v" }, "<leader>dw", function()
 			vim.notify("Added to watches: " .. input, vim.log.levels.INFO)
 		end
 	end
-end, { desc = "add to watches" })
+end, { desc = "Debug add to watches" })
 
 map("n", "<leader>daw", function()
 	local expr = vim.fn.input("Watch expression: ")
@@ -201,7 +201,7 @@ map("n", "<leader>daw", function()
 		require("dapui").elements.watches.add(expr)
 		vim.notify("Added to watches: " .. expr, vim.log.levels.INFO)
 	end
-end, { desc = "add watch (input)" })
+end, { desc = "Debug add watch (input)" })
 
 map("n", "<leader>dcw", function()
 	local watches = require("dapui").elements.watches.get()
@@ -209,7 +209,7 @@ map("n", "<leader>dcw", function()
 		require("dapui").elements.watches.remove(i)
 	end
 	vim.notify("Cleared all watches", vim.log.levels.INFO)
-end, { desc = "clear watches" })
+end, { desc = "Debug clear watches" })
 
 map("n", "<leader>dm", function()
 	local addr = vim.fn.input("Memory address (hex): 0x")
@@ -217,12 +217,74 @@ map("n", "<leader>dm", function()
 		local widgets = require("dap.ui.widgets")
 		widgets.centered_float(widgets.expression("*(void**)0x" .. addr))
 	end
-end, { desc = "view memory" })
+end, { desc = "Debug view memory" })
 
 map("n", "<leader>du", function()
 	require("dapui").toggle()
-end, { desc = "toggle ui" })
+end, { desc = "Debug toggle ui" })
 
 map("n", "<leader>dU", function()
 	require("dapui").open({ reset = true })
-end, { desc = "reset ui" })
+end, { desc = "Debug reset ui" })
+
+-- ═══════════════════════════════════════════════════
+-- Flash
+-- ═══════════════════════════════════════════════════
+map({ "n", "x", "o" }, "s", function()
+	require("flash").jump()
+end, { desc = "Flash jump" })
+
+map({ "n", "x", "o" }, "S", function()
+	require("flash").treesitter()
+end, { desc = "Flash treesitter" })
+
+map("o", "r", function()
+	require("flash").remote()
+end, { desc = "Flash remote" })
+
+map({ "o", "x" }, "R", function()
+	require("flash").treesitter_search()
+end, { desc = "Flash treesitter search" })
+
+map("c", "<c-s>", function()
+	require("flash").toggle()
+end, { desc = "Flash toggle search" })
+
+-- ═══════════════════════════════════════════════════
+-- Noice
+-- ═══════════════════════════════════════════════════
+map("c", "<S-Enter>", function()
+	require("noice").redirect(vim.fn.getcmdline())
+end, { desc = "Noice redirect cmdline" })
+
+map("n", "<leader>snl", function()
+	require("noice").cmd("last")
+end, { desc = "Noice last message" })
+
+map("n", "<leader>snh", function()
+	require("noice").cmd("history")
+end, { desc = "Noice history" })
+
+map("n", "<leader>sna", function()
+	require("noice").cmd("all")
+end, { desc = "Noice all" })
+
+map("n", "<leader>snd", function()
+	require("noice").cmd("dismiss")
+end, { desc = "Noice dismiss all" })
+
+map("n", "<leader>snt", function()
+	require("noice").cmd("pick")
+end, { desc = "Noice picker" })
+
+map({ "i", "n", "s" }, "<c-f>", function()
+	if not require("noice.lsp").scroll(4) then
+		return "<c-f>"
+	end
+end, { silent = true, expr = true, desc = "Noice scroll forward" })
+
+map({ "i", "n", "s" }, "<c-b>", function()
+	if not require("noice.lsp").scroll(-4) then
+		return "<c-b>"
+	end
+end, { silent = true, expr = true, desc = "Noice scroll backward" })
