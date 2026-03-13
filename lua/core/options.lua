28dbaa97e2327d78f3c4864c83671bf4ad845a17
Aiden Yuan <Aiden.Yuan.Eng@gmail.com>
2026-03-13 15:54:00 +0800

    Feat: add  OSC 52

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -5,3 +5,15 @@ local o = vim.o
 o.relativenumber = true
 
 vim.g.vscode_snippets_path = vim.fn.stdpath("config") .. "/snippets"
+
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	},
+}
